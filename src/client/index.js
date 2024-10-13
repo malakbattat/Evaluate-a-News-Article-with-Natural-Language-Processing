a836a0cedafa9a3696be0409b058d7f5af8544bc
Malaak <128179968+malakbattat@users.noreply.github.com>
2024-10-13 09:15:18 +0300

    Add files via upload

---
+++ src/client/index.js
@@ -0,0 +1,7 @@
+import { handleSubmit } from './js/formHandler';
+import './styles/base.scss';
+import './styles/header.scss';
+import './styles/form.scss';
+import './styles/resets.scss';
+
+document.getElementById('form').addEventListener('submit', handleSubmit);
