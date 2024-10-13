fe26edf49c8ff3e057db8713fae4785b276f167c
Malaak <128179968+malakbattat@users.noreply.github.com>
2024-10-13 09:07:35 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,43 @@
+# NLP News Evaluator
+
+## Description
+A web app that evaluates news articles using the MeaningCloud Sentiment Analysis API to display polarity, subjectivity, and a text snippet.
+
+## Instructions
+1. Install dependencies:
+    ```bash
+    npm install
+    ```
+
+2. Run development server:
+    ```bash
+    npm run build-dev
+    ```
+
+3. Build production:
+    ```bash
+    npm run build-prod
+    ```
+
+4. Start production server:
+    ```bash
+    npm run start
+    ```
+
+## Dependencies
+- express
+- webpack
+- jest
+- body-parser
+- cors
+- node-fetch
+- webpack-cli
+- html-webpack-plugin
+- babel-loader
+- sass-loader
+- node-sass
+- dotenv
+- css-loader
+- style-loader
+- workbox-webpack-plugin
+- MeaningCloud API
---
+++ webpack.dev.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+module.exports = {
+  mode: "development",
+  devtool: "source-map",
+  entry: "./src/client/index.js",
+  output: {
+    libraryTarget: 'var',
+    library: 'Client'
+  },
+    module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.json$/,
+        type: "json",
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+    new CleanWebpackPlugin({
+      dry: true,
+      verbose: true,
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false,
+    }),
+  ],
+};
