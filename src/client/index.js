d2bd7ff658d7c260079ea75fd0db70c1c2ebd5c6
Malaak <128179968+malakbattat@users.noreply.github.com>
2024-10-13 09:14:51 +0300

    Delete src directory

--- src/client/index.js
+++
@@ -1,7 +0,0 @@
-import { handleSubmit } from './js/formHandler';
-import './styles/base.scss';
-import './styles/header.scss';
-import './styles/form.scss';
-import './styles/resets.scss';
-
-document.getElementById('form').addEventListener('submit', handleSubmit);
